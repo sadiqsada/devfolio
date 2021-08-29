f103ad0d7c14695fc9a334608410699d757c9bbf
Md Sadiq Sada <samin.sada@gmail.com>
2021-08-29 00:30:42 -0400

    add my information

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,58 +1,56 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://sadiqsada.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Sadiq Sada',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Sadiq Sada | Full-Stack Developer`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `SDE Intern @ Amazon | CS + Applied Math at Stony Brook University`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    // author: `@rfitzio`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/sadiqsada`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/md-sadiq-sada-49845a177/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `Hello! I'm Sadiq, a computer science and applied math double major at Stony Brook University. 
+    I recently completed my Software Development internship at Amazon, where I worked on a project about network connectivity. 
+    I implemented features to ensure a flawless experience for the customers by gracefully 
+    handling poor internet scenarios. Apart from sharpening my computer science skills, I like to play sports and 
+    read books during my free time. I'm currently looking for a full-time opportunity starting in June 2022.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
-        name: 'Devfolio',
+        name: 'PokeUnbox',
         description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'A fun pokemon card simulator + inventory - built with ReactJS and Ant Design',
+        link: 'https://github.com/sadiqsada/PokeUnbox',
       },
       {
-        name: 'ChromeExtensionKit',
+        name: 'Workout Planner',
         description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
+          'An easy way to save, edit, and manage workout sessions - built with Android Studio (Java)',
         link: 'https://chromeextensionkit.com/?ref=devfolio',
-      },
-      {
-        name: 'Another Cool Project',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
+      }
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Amazon',
+        description: 'Software Development Engineer Intern, June 2021 - August 2021',
+        link: ''
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Stony Brook University',
+        description: 'Undergraduate Teaching Assistant (CSE 101: Computer Science Principles), August 2021 - Present',
+        link: ''
       },
       {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Mathnasium',
+        description: 'Instructor, March 2019 - August 2019',
+        link: ''
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
@@ -60,16 +58,16 @@ module.exports = {
       {
         name: 'Languages & Frameworks',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'JavaScript (ES6+), TypeScript, Node.js, Express.js, React, Vue, Vuex',
       },
       {
         name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+        description: 'MongoDB, PostreSQL, Firebase',
       },
       {
         name: 'Other',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'Java, C, Python, Android Studio, XCode, Amazon Web Services (AWS), Agile / Scrum, Git',
       },
     ],
   },
